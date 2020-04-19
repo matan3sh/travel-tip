ecca0f9b790c4aed6234fe24cbd16d9dc14a4dbf
MaxDBam <62358630+MaxDBam@users.noreply.github.com>
2020-04-19 21:46:20 +0300

    Getting metric units from the weather API

--- js/services/weatherService.js
+++ js/services/weatherService.js
@@ -5,17 +5,17 @@ export const weatherService = {
 function getWeather(coords, cityName) {
   const API_KEY = '744c0d7ccc28176f50864df53e6f789c';
   const prmRes = axios.get(
-    `https://api.openweathermap.org/data/2.5/weather?lat=${coords.lat}&lon=${coords.lng}&APPID=${API_KEY}`
+    `https://api.openweathermap.org/data/2.5/weather?lat=${coords.lat}&lon=${coords.lng}&units=metric&APPID=${API_KEY}`
   );
   const prmData = prmRes.then((res) => {
     let locationName = cityName;
     let mainWeather = res.data.weather[0].main;
     let desc = res.data.weather[0].description;
-    let currentTemp = Math.floor(res.data.main.temp / 12.45) + '°c';
-    let maxTemp = Math.floor(res.data.main.temp_max / 12.45) + '°c';
-    let minTemp = Math.floor(res.data.main.temp_min / 12.45) + '°c';
+    let currentTemp = res.data.main.temp + ' °C';
+    let maxTemp = res.data.main.temp_max + ' °C';
+    let minTemp = res.data.main.temp_min + ' °C';
     var icon = res.data.weather[0].icon;
-    var wind = Math.floor(res.data.wind.speed * 3.6) + 'k/h';
+    var wind = res.data.wind.speed + ' m/s';
     return {
       locationName,
       mainWeather,
